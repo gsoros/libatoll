8c1714123587f4dc3ffd8c419f537cb40559b0e3
G Soros <gsoros@users.noreply.github.com>
2022-03-11 19:15:36 +0100

    .

--- include/atoll_touch.h
+++ include/atoll_touch.h
@@ -8,22 +8,30 @@
 namespace Atoll {
 struct TouchPad : public Task {
     int pin = -1;
-    uint16_t threshold = 40;
+    uint16_t threshold = 42;
     bool isTouched = false;
     bool wasTouched = false;
     ulong lastTouched = 0;
 };
 
-#define ATOLL_TOUCH_NUM_PADS 1
+#ifndef ATOLL_TOUCH_NUM_PADS
+#define ATOLL_TOUCH_NUM_PADS 4
+#endif
 
 extern void IRAM_ATTR touchISR0();
+extern void IRAM_ATTR touchISR1();
+extern void IRAM_ATTR touchISR2();
+extern void IRAM_ATTR touchISR3();
 
 class Touch : public Task {
    public:
     static struct TouchPad pads[ATOLL_TOUCH_NUM_PADS];
 
-    Touch(int pin0) {
+    Touch(int pin0 = -1, int pin1 = -1, int pin2 = -1, int pin3 = -1) {
         pads[0].pin = pin0;
+        pads[1].pin = pin1;
+        pads[2].pin = pin2;
+        pads[3].pin = pin3;
     }
 
     void setup() {
@@ -55,7 +63,18 @@ class Touch : public Task {
     }
 
     void attachInterrupts() {
+#if 0 < ATOLL_TOUCH_NUM_PADS
         attachInterrupt(0, touchISR0);
+#endif
+#if 1 < ATOLL_TOUCH_NUM_PADS
+        attachInterrupt(1, touchISR1);
+#endif
+#if 2 < ATOLL_TOUCH_NUM_PADS
+        attachInterrupt(2, touchISR2);
+#endif
+#if 3 < ATOLL_TOUCH_NUM_PADS
+        attachInterrupt(3, touchISR3);
+#endif
     }
 
     void attachInterrupt(uint8_t index, void (*ISR)()) {
--- src/atoll_touch.cpp
+++ src/atoll_touch.cpp
@@ -2,11 +2,21 @@
 
 using namespace Atoll;
 
-// struct TouchPad Atoll::touchPads[ATOLL_TOUCH_NUM_PADS];
-
 struct TouchPad Atoll::Touch::pads[ATOLL_TOUCH_NUM_PADS];
 
 void IRAM_ATTR Atoll::touchISR0() {
     Atoll::Touch::pads[0].isTouched = true;
     Atoll::Touch::pads[0].lastTouched = millis();
+}
+void IRAM_ATTR Atoll::touchISR1() {
+    Atoll::Touch::pads[1].isTouched = true;
+    Atoll::Touch::pads[1].lastTouched = millis();
+}
+void IRAM_ATTR Atoll::touchISR2() {
+    Atoll::Touch::pads[2].isTouched = true;
+    Atoll::Touch::pads[2].lastTouched = millis();
+}
+void IRAM_ATTR Atoll::touchISR3() {
+    Atoll::Touch::pads[3].isTouched = true;
+    Atoll::Touch::pads[3].lastTouched = millis();
 }
