6cc0c3d91a101180e51813318cd27902714b3d73
G Soros <gsoros@users.noreply.github.com>
2022-10-15 20:24:39 +0200

    .

--- include/atoll_task.h
+++ include/atoll_task.h
@@ -33,82 +33,17 @@ class Task {
     virtual void taskStart(float freq = -1,
                            uint32_t stack = 0,
                            int8_t priority = -1,
-                           int8_t core = -1) {
-        if (0 < freq) _taskFreq = freq;
-        if (stack != 0) _taskStack = stack;
-        if (0 <= priority) _taskPriority = (uint8_t)priority;
-        if (0 <= core) _taskCore = (uint8_t)core;
-        if (taskRunning()) {
-            log_w("stopping %s", taskName());
-            taskStop();
-        }
-        _taskSetFreqAndDelay(_taskFreq);
-        uint32_t heap = xPortGetFreeHeapSize();
-        BaseType_t err = xTaskCreatePinnedToCore(
-            _taskLoop,
-            taskName(),
-            _taskStack,
-            this,
-            _taskPriority,
-            &taskHandle,
-            _taskCore);
-        if (pdPASS != err) {
-            log_e("Failed to start task '%s', error %d", taskName(), err);
-            return;
-        }
-        log_i("Started task '%s' at %.2fHz (delay: %dms), stack: %d, heap: %d before, %d after",
-              taskName(), _taskFreq, _taskDelayMs(), _taskStack, heap, xPortGetFreeHeapSize());
-    }
+                           int8_t core = -1);
+    virtual bool taskRunning();
+    virtual void taskStop();
+    virtual void taskSetFreq(const float freq);
+    virtual void taskSetDelay(const uint16_t delayMs);
+    virtual ulong taskGetNextWakeTimeMs();
+    virtual int taskGetLowestStackLevel();
+    virtual void loop();
 
-    virtual bool taskRunning() {
-        return NULL != taskHandle;
-    }
-
-    virtual void taskStop() {
-        if (!taskRunning()) {
-            log_i("%s is not running", taskName());
-            return;
-        }
-        log_i("Stopping %s", taskName());
-        if (NULL == taskHandle) {
-            log_w("%s taskHandle is null", taskName());
-            return;
-        }
-        TaskHandle_t t = taskHandle;
-        taskHandle = NULL;
-        vTaskDelete(t);
-    }
-
-    virtual void taskSetFreq(const float freq) {
-        if (freq <= 0.0f) {
-            log_e("not setting zero freq");
-            return;
-        }
-        _taskSetFreqAndDelay(freq);
-        _taskDebug("taskSetFreq");
-    }
-
-    virtual void taskSetDelay(const uint16_t delayMs) {
-        if (0 == delayMs) {
-            log_w("setting zero delay");
-        }
-        _taskSetFreqAndDelay(delayMs);
-        _taskDebug("taskSetDelay");
-    }
-
-    ulong taskGetNextWakeTimeMs() {
-        return pdTICKS_TO_MS(_taskNextWakeTime);
-    }
-
-    virtual int taskGetLowestStackLevel() {
-        return NULL == taskHandle
-                   ? -1
-                   : (int)uxTaskGetStackHighWaterMark(taskHandle);
-    }
-
-    virtual void loop(){};
-    Task(){};
-    virtual ~Task() {}
+    Task();
+    virtual ~Task();
 
    protected:
     float _taskFreq = ATOLL_TASK_DEFAULT_FREQ;
@@ -119,68 +54,14 @@ class Task {
     TickType_t _taskNextWakeTime = 0;
     TickType_t _taskDelay = 0;
 
-    static void _taskLoop(void *p) {
-        Task *thisPtr = (Task *)p;
-        thisPtr->_taskLastWakeTime = xTaskGetTickCount();
-        for (;;) {
-            // log_i("%s", thisPtr->taskName());
-            ulong start = millis();
-            thisPtr->loop();
-            ulong loopTime = millis() - start;
-            if (thisPtr->_taskDelay < loopTime)
-                // log_w("%s loop time %dms > taskDelay %dms (taskFreq %.2fHz)",
-                //       thisPtr->taskName(), loopTime,
-                //       pdTICKS_TO_MS(thisPtr->_taskDelay), thisPtr->_taskFreq);
-                thisPtr->_taskLastWakeTime = xTaskGetTickCount();
-            thisPtr->_taskNextWakeTime = thisPtr->_taskLastWakeTime + thisPtr->_taskDelay;
-            // log_i("%s delaying for %dms", thisPtr->taskName(),
-            //       pdTICKS_TO_MS(thisPtr->_taskNextWakeTime) - millis());
-            vTaskDelayUntil(&(thisPtr->_taskLastWakeTime), thisPtr->_taskDelay);
-        }
-    }
-
-    void _taskSetFreqAndDelay(const float freq) {
-        _taskFreq = 0.0001f < freq ? freq : 0.0001f;
-        if (_taskFreq <= 0.0001f) {
-            log_w("%s freq is zero, setting max delay", taskName());
-            _taskDelay = (TickType_t)UINT32_MAX;
-        } else
-            _taskDelay = pdMS_TO_TICKS(1000 / _taskFreq);
-        _taskAbortDelay();
-    }
-
-    void _taskSetFreqAndDelay(const TickType_t delayTicks) {
-        _taskDelay = 0 < delayTicks ? delayTicks : 1;
-        ulong ms = _taskDelayMs();
-        if (0 == ms) {
-            log_w("%s delay is zero ms, setting max freq", taskName());
-            _taskFreq = 1000.0f;
-        } else
-            _taskFreq = 1000.0 / ms;
-        _taskAbortDelay();
-    }
-
-    void _taskSetFreqAndDelay(const uint16_t delayMs) {
-        _taskSetFreqAndDelay(pdMS_TO_TICKS(delayMs));
-    }
-
-    void _taskAbortDelay() {
-        if (NULL == taskHandle) {
-            // log_w("taskHandle is null");
-            return;
-        }
-        _taskDebug("_taskAbortDelay");
-        xTaskAbortDelay(taskHandle);
-    }
-
-    void _taskDebug(const char *tag = "") {
-        // log_i("%s %s freq: %.2f, delay: %d", tag, taskName(),
-        //       _taskFreq, _taskDelayMs());
-    }
-
-    ulong _taskDelayMs() {
-        return pdTICKS_TO_MS(_taskDelay);
-    }
+    static void _taskLoop(void *p);
+    void _taskSetFreqAndDelay(const float freq);
+    void _taskSetFreqAndDelay(const TickType_t delayTicks);
+    void _taskSetFreqAndDelay(const uint16_t delayMs);
+    void _taskSetDelay();
+    void _taskAbortDelay();
+    void _taskDebug(const char *tag = "");
+    ulong _taskDelayMs();
 };
 
 }  // namespace Atoll
---
+++ src/atoll_task.cpp
@@ -0,0 +1,160 @@
+#include "atoll_task.h"
+
+using namespace Atoll;
+
+void Task::taskStart(float freq,
+                     uint32_t stack,
+                     int8_t priority,
+                     int8_t core) {
+    if (0 < freq) _taskFreq = freq;
+    if (stack != 0) _taskStack = stack;
+    if (0 <= priority) _taskPriority = (uint8_t)priority;
+    if (0 <= core) _taskCore = (uint8_t)core;
+    if (taskRunning()) {
+        log_w("stopping %s", taskName());
+        taskStop();
+    }
+    _taskSetFreqAndDelay(_taskFreq);
+    uint32_t heap = xPortGetFreeHeapSize();
+    BaseType_t err = xTaskCreatePinnedToCore(
+        _taskLoop,
+        taskName(),
+        _taskStack,
+        this,
+        _taskPriority,
+        &taskHandle,
+        _taskCore);
+    if (pdPASS != err) {
+        log_e("Failed to start task '%s', error %d", taskName(), err);
+        return;
+    }
+    log_i("Started task '%s' at %.2fHz (delay: %dms), stack: %d, heap: %d before, %d after",
+          taskName(), _taskFreq, _taskDelayMs(), _taskStack, heap, xPortGetFreeHeapSize());
+}
+
+bool Task::taskRunning() {
+    return NULL != taskHandle;
+}
+
+void Task::taskStop() {
+    if (!taskRunning()) {
+        log_i("%s is not running", taskName());
+        return;
+    }
+    log_i("Stopping %s", taskName());
+    if (NULL == taskHandle) {
+        log_w("%s taskHandle is null", taskName());
+        return;
+    }
+    TaskHandle_t t = taskHandle;
+    taskHandle = NULL;
+    vTaskDelete(t);
+}
+
+void Task::taskSetFreq(const float freq) {
+    if (freq <= 0.0f) {
+        log_e("%s not setting zero freq", taskName());
+        return;
+    }
+    _taskSetFreqAndDelay(freq);
+    _taskDebug("taskSetFreq");
+}
+
+void Task::taskSetDelay(const uint16_t delayMs) {
+    if (0 == delayMs) {
+        log_w("%s setting zero delay", taskName());
+    }
+    _taskSetFreqAndDelay(delayMs);
+    _taskDebug("taskSetDelay");
+}
+
+ulong Task::taskGetNextWakeTimeMs() {
+    return pdTICKS_TO_MS(_taskNextWakeTime);
+}
+
+int Task::taskGetLowestStackLevel() {
+    return NULL == taskHandle
+               ? -1
+               : (int)uxTaskGetStackHighWaterMark(taskHandle);
+}
+
+void Task::loop(){};
+Task::Task(){};
+Task::~Task() {}
+
+void Task::_taskLoop(void *p) {
+    Task *thisPtr = (Task *)p;
+    thisPtr->_taskLastWakeTime = xTaskGetTickCount();
+    for (;;) {
+        // log_i("%s", thisPtr->taskName());
+        ulong start = millis();
+        thisPtr->loop();
+        ulong loopTime = millis() - start;
+        if (thisPtr->_taskDelay < loopTime) {
+            // log_w("%s loop time %dms > taskDelay %dms (taskFreq %.2fHz)",
+            //       thisPtr->taskName(), loopTime,
+            //       pdTICKS_TO_MS(thisPtr->_taskDelay), thisPtr->_taskFreq);
+        }
+        thisPtr->_taskLastWakeTime = xTaskGetTickCount();
+        thisPtr->_taskNextWakeTime = thisPtr->_taskLastWakeTime + thisPtr->_taskDelay;
+        // log_i("%s delaying for %dms", thisPtr->taskName(),
+        //       pdTICKS_TO_MS(thisPtr->_taskNextWakeTime) - millis());
+        xTaskDelayUntil(&(thisPtr->_taskLastWakeTime), thisPtr->_taskDelay);
+    }
+}
+
+void Task::_taskSetFreqAndDelay(const float freq) {
+    _taskFreq = 0.0001f < freq ? freq : 0.0001f;
+    if (_taskFreq <= 0.0001f) {
+        log_w("%s freq is zero, setting max delay", taskName());
+        _taskDelay = (TickType_t)UINT32_MAX;
+    } else
+        _taskDelay = pdMS_TO_TICKS(1000 / _taskFreq);
+    //_taskAbortDelay();
+    _taskSetDelay();
+}
+
+void Task::_taskSetFreqAndDelay(const TickType_t delayTicks) {
+    _taskDelay = 0 < delayTicks ? delayTicks : 1;
+    ulong ms = _taskDelayMs();
+    if (0 == ms) {
+        log_w("%s delay is zero ms, setting max freq", taskName());
+        _taskFreq = 1000.0f;
+    } else {
+        // log_i("freq: %f", 1000.0 / ms);
+        _taskFreq = 1000.0 / ms;
+    }
+    //_taskAbortDelay();
+    _taskSetDelay();
+}
+
+void Task::_taskSetFreqAndDelay(const uint16_t delayMs) {
+    _taskSetFreqAndDelay(pdMS_TO_TICKS(delayMs));
+}
+
+void Task::_taskSetDelay() {
+    TickType_t t = pdMS_TO_TICKS(millis());
+    TickType_t lastWake = t < _taskLastWakeTime ? 0 : t - _taskLastWakeTime;
+    TickType_t delay = _taskDelay <= lastWake ? 1 : _taskDelay - lastWake;
+    // log_i("delaying %s for %dms", taskName(), pdTICKS_TO_MS(delay));
+    _taskNextWakeTime = _taskLastWakeTime + delay;
+    xTaskDelayUntil(&_taskLastWakeTime, delay);
+}
+
+void Task::_taskAbortDelay() {
+    if (NULL == taskHandle) {
+        // log_w("taskHandle is null");
+        return;
+    }
+    _taskDebug("_taskAbortDelay");
+    xTaskAbortDelay(taskHandle);
+}
+
+void Task::_taskDebug(const char *tag) {
+    // log_i("%s %s freq: %.2f, delay: %d", tag, taskName(),
+    //       _taskFreq, _taskDelayMs());
+}
+
+ulong Task::_taskDelayMs() {
+    return pdTICKS_TO_MS(_taskDelay);
+}
